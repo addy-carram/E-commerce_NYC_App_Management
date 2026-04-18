3239f153bb5fc2c5b7abcbd5c0d71b285e443f98
Addy <addycarram@gmail.com>
2026-04-18 21:41:57 +0300

    finishing the project changing  icons a little bit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -118,10 +118,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(18, 25);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 27);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(138, 48);
+            this.pictureBox1.Size = new System.Drawing.Size(234, 48);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
